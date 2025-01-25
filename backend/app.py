d90c892a5ba991c9177452b1f511d8aaf746494b
unknown <premsingh192002@gmail.com>
2025-01-25 18:39:07 +0530

    second

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ CORS(app)  # Enable CORS for all routes
 def deploy():
     data = request.json
     repo_url = data['repoUrl']
-    
+
     # Remove the 'app' directory if it exists to ensure a clean start
     if os.path.exists('app'):
         print("Removing existing 'app' directory...")
@@ -24,8 +24,8 @@ def deploy():
     print("Git Clone stderr:", clone_process.stderr)
 
     if clone_process.returncode != 0:
-        return jsonify({'message': f'Failed to clone the repository: {clone_process.stderr}'}), 500
-    
+	 return jsonify({'message': f'Failed to clone the repository: {clone_process.stderr}'}), 500
+
     print("Building Docker image...")
     build_process = subprocess.run("cd app && docker build -t myapp .", shell=True, text=True, capture_output=True)
     print("Docker Build stdout:", build_process.stdout)
@@ -36,7 +36,7 @@ def deploy():
 
     # Push the image to AWS ECR (Assume credentials are set)
     print("Tagging the Docker image...")
-    tag_process = subprocess.run("docker tag myapp:latest 992382487320.dkr.ecr.ap-south-1.amazonaws.com/myapp:latest", 
+    tag_process = subprocess.run("docker tag myapp:latest 992382487320.dkr.ecr.ap-south-1.amazonaws.com/myapp:latest",
                                  shell=True, text=True, capture_output=True)
     print("Docker Tag stdout:", tag_process.stdout)
     print("Docker Tag stderr:", tag_process.stderr)
@@ -44,8 +44,8 @@ def deploy():
     if tag_process.returncode != 0:
         return jsonify({'message': f'Failed to tag the Docker image: {tag_process.stderr}'}), 500
 
-    print("Pushing the Docker image to AWS ECR...")
-    push_process = subprocess.run("docker push 992382487320.dkr.ecr.ap-south-1.amazonaws.com/myapp:latest", 
+print("Pushing the Docker image to AWS ECR...")
+    push_process = subprocess.run("docker push 992382487320.dkr.ecr.ap-south-1.amazonaws.com/myapp:latest",
                                   shell=True, text=True, capture_output=True)
     print("Docker Push stdout:", push_process.stdout)
     print("Docker Push stderr:", push_process.stderr)
@@ -55,5 +55,5 @@ def deploy():
 
     return jsonify({'message': 'Deployment started successfully'}), 200
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
     app.run(debug=True)
